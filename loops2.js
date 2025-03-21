826f077e1d71ae458beffe1cb116c526c64bc53a
tomoarmand <tomo.armand@gmail.com>
2025-03-21 16:34:01 +1300

    Update loops2.js

--- loops2.js
+++ loops2.js
@@ -1,10 +1,13 @@
+const numbers = [1, 2, 3, 4, 5];
 
+function convertToStrings(arr) {
+    return arr.map(function(el) {
+       return el.toString();
+    })
+  
+}
 
-const words = ["hello", "world", "javascript", "arrays"];
-const uppercasedWords = [];
-
-function upperCasedWords()
-
-
-
-console.log(uppercasedWords)
+// Test the function
+const stringArray = convertToStrings(numbers);
+console.log(stringArray); // Should output: ["1", "2", "3", "4", "5"]
+console.log(numbers); // Should remain unchanged: [1, 2, 3, 4, 5]
