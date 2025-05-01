d55215ad0f23b3a30bd0abffd8cd7866ff0913b7
tomoarmand <tomo.armand@gmail.com>
2025-05-01 16:42:37 +1200

    vh

--- react/weather-app-class/src/components/WeatherPage.jsx
+++ react/weather-app-class/src/components/WeatherPage.jsx
@@ -6,6 +6,7 @@ import WeatherDetails from "./WeatherDetails";
 
 function WeatherPage() {
     const [weather, setWeather] = useState();
+    const [contact, setContact] = useState();
 
     const weatherData = async () => {
         const response = await fetch("http://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m");
@@ -15,13 +16,29 @@ function WeatherPage() {
         setWeather(data);
     }
 
+    const contactData = async () => {
+        const response = await fetch("http://localhost:3000/contacts");
+
+        // console.log(await response.json())
+        const data = await response.json();
+        setContact(data);
+    }
+
     useEffect(() => {
         weatherData();
+        contactData()
     }, [])
     
     return (
         <>
+        <div>
         {weather && <WeatherDetails weather={weather}/>}
+        {/* {contact && <div>{contact.name}</div>}
+        {contact && <div>{contact.occupation}</div>} */}
+        <ul>
+            {contact.map}
+        </ul>
+        </div>
         </>
     )
 }
