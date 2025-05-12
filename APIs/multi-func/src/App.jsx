8d3b163104bdb33e95c4c1cae74fa7241d6b9a38
tomoarmand <tomo.armand@gmail.com>
2025-05-13 09:45:39 +1200

    up

--- APIs/multi-func/src/App.jsx
+++ APIs/multi-func/src/App.jsx
@@ -4,11 +4,13 @@ import './App.css'
 function App() {
   const [quote, setQuote] = useState(null);
   const [answer, setAnswer] = useState(null);
-  const [meal, setMeal] = useState(null)
-  const [numberFact, setNumberFact] = useState(null)
+  const [meal, setMeal] = useState(null);
+  const [numberFact, setNumberFact] = useState(null);
+  const [audioUrl, setAudioUrl] = useState(null);
+  const [birdRecording, setBirdRecording] = useState(null);
 
   const fetchQuotes = async () => {
-    
+
     const response = await fetch("https://gomezmig03.github.io/MotivationalAPI/en.json");
 
     const data = await response.json();
@@ -33,9 +35,20 @@ function App() {
     setMeal(data)
   }
 
-  // const fetchBirdSounds = async () => {
-  //   const response
-  // }
+  const fetchBirdSounds = async () => {
+    const response = await fetch("https://xeno-canto.org/api/2/recordings?query=cnt:brazil");
+    const data = await response.json()
+
+    console.log(data.recordings)
+
+    const randomIndex = Math.floor(Math.random() * data.recordings.length)
+
+    console.log(data.recordings[randomIndex].id)
+
+    setBirdRecording(data.recordings[randomIndex])
+
+    setAudioUrl(`https://xeno-canto.org/${data.recordings[randomIndex].id}/download`)
+  }
 
   const fetchNumberFact = async () => {
     const response = await fetch("http://numbersapi.com/random/math?json");
@@ -44,31 +57,44 @@ function App() {
 
   }
 
- function clearAll() {
-  setAnswer(null);
-  setQuote(null);
-  setMeal(null);
-  setNumberFact(null);
- }
+  function clearAll() {
+    setAnswer(null);
+    setQuote(null);
+    setMeal(null);
+    setNumberFact(null);
+    setAudioUrl(null);
+  }
 
   return (
     <>
-    <button onClick={fetchQuotes}>Get Random Quote</button>
-    {quote && (
-      <>
-    <p><b>{quote && quote.phrase}</b></p>
-    <p>Author: {quote && quote.author}</p>
-      </>
-    )}
-    <button onClick={fetchAnswers}>Answer your question</button>
-    <p>{answer && answer.answer}</p>
-    <button onClick={fetchMeals}>Dinner Idea</button>
-    {meal && meal.meals.map((el) => (
-      <p>{el.strMeal}</p>
-    ))}
-    <button onClick={fetchNumberFact}>Get Number Fact</button>
-    <p>{numberFact && numberFact.text}</p>
-    <button onClick={clearAll}>Clear All</button>
+      <button onClick={fetchQuotes}>Get Random Quote</button>
+      {quote && (
+        <>
+          <p><b>{quote && quote.phrase}</b></p>
+          <p>Author: {quote && quote.author}</p>
+        </>
+      )}
+      <button onClick={fetchAnswers}>Answer your question</button>
+      <p>{answer && answer.answer}</p>
+      <button onClick={fetchMeals}>Dinner Idea</button>
+      {meal && meal.meals.map((el) => (
+        <p>{el.strMeal}</p>
+      ))}
+      <button onClick={fetchNumberFact}>Get Number Fact</button>
+      <p>{numberFact && numberFact.text}</p>
+      <button onClick={clearAll}>Clear All</button>
+      <button onClick={fetchBirdSounds}>Get Brazilian Bird Sound</button>
+      {audioUrl &&
+        <audio controls>
+          <source src={audioUrl} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>}
+      {audioUrl &&
+        <>
+          <p>Genus: {birdRecording.gen}</p>
+          <p>Species: {birdRecording.sp}</p>
+        </>
+      }
     </>
   )
 }
