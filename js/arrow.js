aa08c1f401af0635aa47c9cf092b3824782a566f
tomoarmand <tomo.armand@gmail.com>
2025-06-12 10:17:02 +1200

    o

---
+++ js/arrow.js
@@ -0,0 +1,12 @@
+let square = num => num * num;
+console.log(square(5));
+
+let addTen = num => num + 10;
+console.log(addTen(3));
+
+let upperCase = string => string.toUpperCase;
+console.log(upperCase("hello"));
+
+let numbers = [1, 2, 3, 4,].map(element => element * element);
+console.log(numbers);
+
